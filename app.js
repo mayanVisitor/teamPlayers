577775cc61248e130b7783a43f5ff53b51ea9625
root <root@jb.rga.ip>
2019-03-14 18:54:41 -0700

    For what it's worth

--- app.js
+++ app.js
@@ -9,39 +9,59 @@ app.set("view engine","ejs");
 app.use(parser.urlencoded({extended:true}));
 
 
-mongo.connect("mongodb://localhost:27017/blogs", {useNewUrlParser:true});
+mongo.connect("mongodb://localhost:27017/teams", {useNewUrlParser:true});
 
-var blogSchema = new mongo.Schema({
+var playerSchema = new mongo.Schema({
     name: String,
-    rating: Number,
-    desc: String,
-    crated:Date
+    age: Number,
+    nationality: String,
+    position:String,
+    number:String,
+    creation: Date
     });
+var Player = mongo.model("Player", playerSchema);
 
-var Blog = mongo.model("Blog", blogSchema);
+var teamSchema = new mongo.Schema({
+    name: String,
+    city: String,
+    creation: Date,
+    members:[playerSchema]
+    });
+var Team = mongo.model("Team", teamSchema);
 
 app.get("/",(req, res)=>{
     res.render("home");
 });
 
-app.get("/blogs", (req,res)=>{
-    Blog.find({},(err, allBlogs)=>{
-        res.render("blogs",{allBlogs:allBlogs});
+app.get("/teams", (req,res)=>{
+    Team.find({},(err, allTeams)=>{
+        if(err){
+            console.log(err);
+        }else{
+        res.render("teams",{allTeams:allTeams});}
         });
-    
-});
+    });
 
-app.get("/blogs/new", (req, res)=>{
+app.get("/teams/new", (req, res)=>{
     res.render("new");
     });
 
-app.post("/blogs",(req, res)=>{
-    /*var name=req.body.name;
-    var rating=req.body.rating;
-    var desc=req.body.desc;*/
-    console.log(req);
+app.post("/teams",(req, res)=>{
+    var name_ = req.body.name;
+    var city_ = req.body.city;
+    Team.create({name:name_, city:city_});
+    
+    });
+
+app.get("/teams/:id",(req,res)=>{
+    Team.find({},()=>{
+        
+    })
+        
     });
     
+    
+});
     
     
 app.listen(3000,()=>{console.log("Listening...")});
